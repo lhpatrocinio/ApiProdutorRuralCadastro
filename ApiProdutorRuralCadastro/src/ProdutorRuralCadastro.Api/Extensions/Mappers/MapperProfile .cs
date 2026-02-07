13060b8ccbed28b5ace61ecc15f2c8fd14314d33
Marcelo Morais <marceloms17@gmail.com>
2026-02-07 17:40:28 -0300

    feat(api): add AutoMapper profile
    - Add MapperProfile with Entity to DTO mappings
    - Configure Propriedade, Talhao, Cultura mappings
    - Map nested relationships (Talhao with Propriedade/Cultura)

--- ApiProdutorRuralCadastro/src/ProdutorRuralCadastro.Api/Extensions/Mappers/MapperProfile .cs
+++ ApiProdutorRuralCadastro/src/ProdutorRuralCadastro.Api/Extensions/Mappers/MapperProfile .cs
@@ -1,4 +1,8 @@
 ﻿using AutoMapper;
+using ProdutorRuralCadastro.Application.DTOs.Cultura;
+using ProdutorRuralCadastro.Application.DTOs.Propriedade;
+using ProdutorRuralCadastro.Application.DTOs.Talhao;
+using ProdutorRuralCadastro.Domain.Entities;
 
 namespace ProdutorRuralCadastro.Api.Extensions.Mappers
 {
@@ -6,6 +10,61 @@ namespace ProdutorRuralCadastro.Api.Extensions.Mappers
     {
         public MapperProfile()
         {
+            // Cultura Mappings
+            CreateMap<Cultura, CulturaResponse>();
+
+            // Propriedade Mappings
+            CreateMap<PropriedadeCreateRequest, Propriedade>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Talhoes, opt => opt.Ignore())
+                .ForMember(dest => dest.Ativo, opt => opt.MapFrom(src => true));
+
+            CreateMap<PropriedadeUpdateRequest, Propriedade>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.ProdutorId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Talhoes, opt => opt.Ignore());
+
+            CreateMap<Propriedade, PropriedadeResponse>();
+
+            CreateMap<Propriedade, PropriedadeComTalhoesResponse>();
+
+            // Talhao Mappings
+            CreateMap<TalhaoCreateRequest, Talhao>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Propriedade, opt => opt.Ignore())
+                .ForMember(dest => dest.Cultura, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => 0))
+                .ForMember(dest => dest.StatusDescricao, opt => opt.MapFrom(src => "Normal"))
+                .ForMember(dest => dest.Ativo, opt => opt.MapFrom(src => true));
+
+            CreateMap<TalhaoUpdateRequest, Talhao>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.PropriedadeId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Propriedade, opt => opt.Ignore())
+                .ForMember(dest => dest.Cultura, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore())
+                .ForMember(dest => dest.StatusDescricao, opt => opt.Ignore());
+
+            CreateMap<Talhao, TalhaoResponse>()
+                .ForMember(dest => dest.StatusDescricao, opt => opt.MapFrom(src => 
+                    string.IsNullOrEmpty(src.StatusDescricao) ? src.GetStatusDescricao() : src.StatusDescricao));
+
+            CreateMap<Talhao, TalhaoDetailResponse>()
+                .ForMember(dest => dest.StatusDescricao, opt => opt.MapFrom(src => 
+                    string.IsNullOrEmpty(src.StatusDescricao) ? src.GetStatusDescricao() : src.StatusDescricao))
+                .ForMember(dest => dest.CulturaNome, opt => opt.MapFrom(src => src.Cultura != null ? src.Cultura.Nome : null))
+                .ForMember(dest => dest.PropriedadeNome, opt => opt.MapFrom(src => src.Propriedade != null ? src.Propriedade.Nome : null));
+
+            CreateMap<Talhao, TalhaoResumoResponse>()
+                .ForMember(dest => dest.CulturaNome, opt => opt.MapFrom(src => src.Cultura != null ? src.Cultura.Nome : null));
         }
     }
 }
